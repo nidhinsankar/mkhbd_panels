86a64343bf105efce1757dc48a6c8f85e00bf6bb
nidhinsankar <nidhinsankar1@gmail.com>
2024-12-14 08:20:11 +0530

    add:react-native-vector-icons ,changed the bottom-tab icons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import FIcon from 'react-native-vector-icons/FontAwesome';
 import {StatusBar, StyleSheet} from 'react-native';
 import Profile from './screens/Profile';
 import ForYouScreen from './screens/ForYou';
@@ -13,9 +15,33 @@ function App(): React.JSX.Element {
     <NavigationContainer>
       <StatusBar />
       <Tab.Navigator>
-        <Tab.Screen name="Explore" component={ExploreScreen} />
-        <Tab.Screen name="For You" component={ForYouScreen} />
-        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen
+          name="For You"
+          component={ForYouScreen}
+          options={{
+            tabBarIcon: ({focused, color, size}) => (
+              <Icon name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Explore"
+          component={ExploreScreen}
+          options={{
+            tabBarIcon: ({focused, color, size}) => (
+              <Icon name="search-circle-sharp" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({focused, color, size}) => (
+              <FIcon name="user-circle-o" size={size} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
